884c30e8acf59b5e7bcbccbd6a0b168a8f6678c3
Manimaran Ramakrishnan <61810507+r-manimaran@users.noreply.github.com>
2024-07-04 23:04:41 -0400

    Microsoft Semantic Kernel Console App
    Uses OpenAI and Semantic Kernel

---
+++ console-semantickernel/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Connectors.HuggingFace;
+
+var builder = Kernel.CreateBuilder();
+
+var configBuilder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true)
+    .AddUserSecrets<Program>();
+IConfiguration configuration = configBuilder.Build();
+
+string openAI_Key = configuration["OpenAI_API_Key"];
+string huggingFace_ApiKey = configuration["HUGGINGFACEAPIKEY"];
+
+builder.AddOpenAIChatCompletion("gpt-3.5-turbo",
+                                openAI_Key);
+
+
+//builder.AddHuggingFaceChatCompletion("google/flan-t5-xxl");                                    
+                                     //huggingFace_ApiKey);
+var kernel = builder.Build();
+var results = await kernel.InvokePromptAsync("Give me a list of breakfast foods with their calorie count");
+
+
+Console.WriteLine(results);
+
+Console.ReadKey();
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Use Microsoft Semantic Kernel to access LLM
+
+
+## Define the keys in appSettings or User Secrets
+
+Used User-secrets here. But can define the same keys in appSettings.json and use it in the application
+
+
+## Run the Application
+
+To build & Run
+
+```bash
+    # Build the application
+    dotnet build console-sematickernel.csproj
+    # Run the application
+    dotnet run
+```
+
+## Output
+
+![alt text](image.png)
